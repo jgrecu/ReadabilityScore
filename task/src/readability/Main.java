b4898c900d28e6da88945448f360931a2c8378a9
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-30 14:59:18 +0100

    JBA - Readability Score - Stage 1/4: Simplest estimation

--- task/src/readability/Main.java
+++ task/src/readability/Main.java
@@ -1,7 +1,12 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        final Scanner scanner = new Scanner(System.in);
+        String inputText = scanner.nextLine();
+
+        System.out.print(inputText.length() > 100 ? "HARD" : "EASY");
     }
 }
