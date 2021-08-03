afe5555bd88ea747732217c7561222c6e6b88b25
Jeremy Grecu <jeremy.grecu@sap.com>
2021-08-03 13:51:38 +0100

    JBA - Readability Score - Stage 2/4: Words and sentences

--- task/src/readability/Main.java
+++ task/src/readability/Main.java
@@ -6,7 +6,14 @@ public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         String inputText = scanner.nextLine();
+        String[] phrases = inputText.split("\\.|!|\\?");
+        int textSize = phrases.length;
+        double numOfWords = 0;
 
-        System.out.print(inputText.length() > 100 ? "HARD" : "EASY");
+        for (int i = 0; i < textSize; i++) {
+            numOfWords += phrases[i].trim().split(" ").length;
+        }
+
+        System.out.print(numOfWords / textSize > 10 ? "HARD" : "EASY");
     }
 }
