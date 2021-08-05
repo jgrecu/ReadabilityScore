54545f2cd26af3058e8e683ac915b5b5631b9e69
Jeremy Grecu <jgrecu@gmail.com>
2021-08-05 18:56:41 +0100

    add test

---
+++ src/readability/MainTest.java
@@ -0,0 +1,49 @@
+package readability;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+class MainTest {
+
+    @Test
+    void getText() {
+    }
+
+    @Test
+    void countSyllablesRegex() {
+    }
+
+    String[] words = {"disfranchised"};
+    @Test
+    void countSyllables(String[] words) {
+        assertEquals(5, countSyllables(words));
+    }
+
+    @org.junit.jupiter.api.Test
+    void countPolySyllables() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getARIScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFKScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSMOGScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCLScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateAge() {
+    }
+}
